f06543fc6a9e833dd1b3f8b9dbd9328e1c37ab19
Anlele1701 <lethanhduyan@gmail.com>
2023-12-27 14:54:35 +0700

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
